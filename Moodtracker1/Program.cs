99b31a7af7bea85956e0f32096b3e37a5542293f
Jukka Prami <43521@edu.raseko.fi>
2024-02-01 09:01:41 +0200

    1.2

---
+++ Moodtracker1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moodtracker1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Muuttujat mielialan ja kunnon tallentamiseen
+            int vireys;
+            int uni;
+            int nalka;
+            Console.WriteLine("Hyvää huomenta, miltä Sinusta tuntuu tänään?");
+            Console.WriteLine();
+            Console.WriteLine("Arvioi vireyttäsi asteikolla 1-5 1: unelias, 5: täynnä räjähtävää energiaa");
+            vireys = Console.ReadLine();   
+
+
+            Console.ReadLine();
+        }
+    }
+}
