a29a3f2c0d9090518a3bcf55bdba38921e8f1c7b
Jukka Prami <43521@edu.raseko.fi>
2024-02-13 10:40:05 +0200

    13.2

---
+++ LuokkaHarjoituksia1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luokkaharjoituksia1
+{
+    class Hooman
+    {
+        // Define properties of Hooman ie. fields
+        public string name = "Essi Esimerkki";
+        public int age = 30;
+        public string gender = "Emäntä";
+
+        // Default constructor w/o arguments
+        // No need to define, will be crated automatically
+        public Hooman()
+        {
+
+        }
+
+        // Constructor with one argument
+        public Hooman(string name)
+        {
+            this.name = name;
+        }
+
+        // Constructor with 2 arguments
+        public Hooman(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        // Constructor with 3 arguments
+        public Hooman(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+
+        }
+
+        // A method to say something
+        public void SayOpinion()
+        {
+            Console.WriteLine("Voi, lemmikit ne on elämän suola");
+        }
+    }
+
+    class CatOwner : Hooman
+    {
+        public new void SayOpinion()
+        {
+            Console.WriteLine("Kissat ovat itsenäisiä ja pitävät hiiret loitolla");
+        }
+    }
+
+    class DogOwner : Hooman
+
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create (instantiate) a hooman object from Hooman class
+            Hooman owner = new Hooman("Ossi Omistaja", 35, "isäntä");
+
+            // Call the SayOpinion method
+            owner.SayOpinion();
+
+            // Save owners name property to a variable
+            string who = owner.name;
+
+            // Test it by echoing it to a console
+            Console.WriteLine("totesi " + who);
+
+            // Create a new catowner object
+            CatOwner catOwner = new CatOwner();
+
+            // Use catowners SayOpinion method
+            catOwner.SayOpinion();
+
+            // Keep the window opent until enter pressed
+            Console.ReadLine();
+
+
+        }
+    }
+}
--- Moodtracker1/Program.cs
+++ Moodtracker1/Program.cs
@@ -11,16 +11,42 @@ namespace Moodtracker1
         static void Main(string[] args)
         {
             // Muuttujat mielialan ja kunnon tallentamiseen
+            string vireysSTR;
             int vireys;
+            string uniSTR;
             int uni;
+            string nalkaSTR;
             int nalka;
+            int moodIndex;
+
             Console.WriteLine("Hyvää huomenta, miltä Sinusta tuntuu tänään?");
             Console.WriteLine();
             Console.WriteLine("Arvioi vireyttäsi asteikolla 1-5 1: unelias, 5: täynnä räjähtävää energiaa");
-            vireys = Console.ReadLine();   
+            vireysSTR = Console.ReadLine();
+            Console.WriteLine("Miten hyvin nukuit? 1: huonosti, 5: kuin tukki");
+            uniSTR = Console.ReadLine();
+            Console.WriteLine("Miten nälkäinen olet? 1: kaamea nälkä, 5: ei muruakaan");
+            nalkaSTR = Console.ReadLine();
+
+            // Muutetaan vastaukset numeromuotoon
+            vireys = int.Parse(vireysSTR);
+            uni = int.Parse(uniSTR);
+            nalka = int.Parse(nalkaSTR);
+
+            // Lasketaan mielialaindeksi
+            moodIndex = vireys + uni + nalka;
 
 
+            // Tulostetaan käyttäjän antamat vastaukset
+            Console.WriteLine("Vireyspisteet: " + vireysSTR + ", unipisteet: " + uniSTR + " ja  nälkäpisteet: " + nalkaSTR);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Mielialaindeksisi on: " + moodIndex);
+
+            // pidetään konsoli ikkuna auki, kunnes painetaan <Enter>
             Console.ReadLine();
         }
+
     }
 }
