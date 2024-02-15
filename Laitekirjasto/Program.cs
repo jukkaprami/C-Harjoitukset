fdb2787390279d5076f14a230224565463872907
Jukka Prami <43521@edu.raseko.fi>
2024-02-15 16:11:42 +0200

    15.2 final

--- Laitekirjasto/Program.cs
+++ Laitekirjasto/Program.cs
@@ -7,11 +7,18 @@ using System.Threading.Tasks;
 
 namespace Laitekirjasto
 {
+    // LUOKKAMÄÄRITYKSET
+    // =================
+
     // Yleinen laiteluokka, yliluokka tietokoneille, tableteille ja puhelimille
     class Device
     {
+        // Kentät ja ominaisuudet
+        // ----------------------
+
         // Luodaan kenttä (field) name, esitellään (define) ja annetaan arvo (set initial value)
         string name = "Uusi laite";
+
         // Luodaan kenttää vastaava ominaisuus (property) Name ja sille asetusmetodi set ja lukumetodi get. Ne voi kirjoittaa joko yhdelle tai useammalle riville
         public string Name { get { return name; } set { name = value; } }
 
@@ -35,6 +42,8 @@ namespace Laitekirjasto
         int storageCapacity = 0;
         public int StorageCapacity { get { return storageCapacity; } set { storageCapacity = value; } }
 
+        // Konstruktorit
+        // -------------
 
         // Konstruktori eli olionmuodostin (constructor) ilman argumentteja
         public Device()
@@ -57,7 +66,35 @@ namespace Laitekirjasto
             this.warranty = warranty;
         }
 
+        // Muut metodit
+        // ------------
 
+        // Yliluokan metodit
+        public void ShowPurchaseInfo()
+        {
+            // Luetaan laitteen ostotiedot sen kentistä, huom! this
+            Console.WriteLine();
+            Console.WriteLine("Laitteen hankintatiedot");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Laitteen nimi: " + this.name);
+            Console.WriteLine("Ostopäivä: " + this.purchaseDate);
+            Console.WriteLine("Hankinta: " + this.price);
+            Console.WriteLine("Takuu: " + this.warranty + " kk");
+        }
+
+        // Luetaan laitteen yleiset tekniset tiedot ominaisuuksista, huom iso alkukirjain
+        public void ShowBasicTechnicalInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Laitteen tekniset tiedot");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Koneen nimi: " + Name);
+            Console.WriteLine("Prosessori: " + ProcessorType);
+            Console.WriteLine("Keskusmuisti: " + AmountRam);
+            Console.WriteLine("Levytila: " + StorageCapacity);
+
+
+        }
 
     }
 
@@ -74,54 +111,93 @@ namespace Laitekirjasto
         { }
 
         // Muut metodit
-        public void ShowInfo()
-        {
-            Console.WriteLine("Koneen nimi: " + Name);
-            Console.WriteLine("Prosessori: " + ProcessorType);
-            Console.WriteLine("Keskusmuisti: " + AmountRam);
-            Console.WriteLine("Levytila: " + StorageCapacity);
-        }
+
     }
 
     // Tablettien luokka, perii laiteluokan
     class Tablet : Device
     {
-        // Kentät
+        // Kentät ja ominaisuudet
+        // ----------------------
+
         string operatingSystem;
         public string OperatingSystem { get { return operatingSystem; } set { operatingSystem = value; } }
         bool stylusEnabled = false;
         public bool StylusEnabled { get { return stylusEnabled; } set { stylusEnabled = value; } }
+
+        // Konstruktortit
+        // --------------
+
+        public Tablet() : base() { }
+
+        public Tablet(string name) : base(name) { }
+
+
+        // Tablet-luokan erikoismetodit
+        // ----------------------------
+        public void TabletInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Tabletin erityitiedot");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Käyttöjärjestelmä: " + OperatingSystem);
+            Console.WriteLine("Kynätuki: " + StylusEnabled);
+        }
+
     }
     // Pääohjelman luokka, josta tulee Program.exe
+    // ===========================================
     internal class Program
     {
+        // Ohjelman käynnistävä metodi
+        // ---------------------------
         static void Main(string[] args)
         {
-            // Ohjelman varsinaiset toiminnot tapahtuvat täällä
-            // Ohjelma kysyy käyttäjältä tietoja laitteista ja
-            // vastaamalla kysymyksiin tiedot tallennetaan muuttujiin.
-
-            // Luodaan uusi laite Device-luokasta
-
-            Device laite = new Device("Opekone");
-            Console.WriteLine("Laitteen nimi on: " + laite.Name);
-            Console.WriteLine("Ostopäivä oli: " + laite.PurchaseDate);
+            // Olioiden luominen ja metodien testaus
+            // -------------------------------------
 
             // Luodaan uusi tietokone, joka perii laiteluokan (Device) ominaisuudet ja metodit
+
             Computer tietokone1 = new Computer();
 
-            // Asetetaan Prosessori-ominaisuuden arvo
+            // Asetetaan ensimmäisen tietokoneen ominaisuuksien arvot
             tietokone1.ProcessorType = "Intel I7";
             tietokone1.AmountRam = 16;
+            tietokone1.PurchaseDate = "15.2.2024";
+            tietokone1.Price = 850.00d;
+            tietokone1.Warranty = 36;
 
-            Console.WriteLine("Uuden tietokoneen nimi on: " + tietokone1.Name + " ja siinä on " + tietokone1.ProcessorType + " -prosessori ja " + tietokone1.AmountRam + " GB keskusmuistia");
+            Console.WriteLine("Tietokone 1");
+            Console.WriteLine("===========");
+            tietokone1.ShowPurchaseInfo();
 
-            // Luodaan uusi nimetty tietokone toisella konstruktorilla
+
+            // Luodaan uusi nimetty tietokone 
             Computer tietokone2 = new Computer("Mikan läppäri");
             tietokone2.ProcessorType = "Intel Core i7 vPro";
             tietokone2.AmountRam = 32;
-            tietokone2.ShowInfo();
 
+            Console.WriteLine("Tietokone 2");
+            Console.WriteLine("===========");
+            tietokone2.ShowBasicTechnicalInfo();
+
+            // Luodaan testiolio tabletille
+
+            Tablet tabletti1 = new Tablet("Mikan iPad");
+            tabletti1.PurchaseDate = "1.10.2022";
+            tabletti1.OperatingSystem = "IOS";
+            tabletti1.StylusEnabled = true;
+
+            // Näytetään tietoja metodien avulla
+
+            tabletti1.ShowPurchaseInfo();
+
+
+            tabletti1.ShowBasicTechnicalInfo();
+
+            Console.WriteLine("Tabletti 1");
+            Console.WriteLine("==========");
+            tabletti1.TabletInfo();
 
 
             // Pidetään ikkuna auki, kunnes käyttäjä painaa <enter>
